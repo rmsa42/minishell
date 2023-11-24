fc1840d900e062c8352b68027ea42ffc45a275f0
Diogo Andre Dos Santos Correia <diogo_correia7@hotmail.com>
2023-11-24 08:38:25 +0000

    123

--- builtin.c
+++ builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:04:33 by rumachad          #+#    #+#             */
-/*   Updated: 2023/11/10 15:42:01 by rumachad         ###   ########.fr       */
+/*   Updated: 2023/11/23 12:34:48 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	pwd(void);
 void	cd(t_minishell *shell);
 void	echo(t_minishell *shell);
-void	export(t_env *env, char **cmd_split);
+void	export(t_env *env, char *cmd_split);
 void	unset(t_env *env, char *cmd);
 void	env(t_env *env);
 
@@ -32,7 +32,7 @@ void	builtin_cmd(t_minishell *shell)
 	else if (!ft_strncmp(shell->cmd_split[0], "unset", 6))
 		unset(shell->env, shell->cmd_split[1]);
 	else if (!ft_strncmp(shell->cmd_split[0], "export", 7))
-		export(shell->env, shell->cmd_split);
+		export(shell->env, shell->cmd_split[1]);
 	else if (!ft_strncmp(shell->cmd_split[0], "exit", 5))
 	{
 		clean_program(shell);
--- export.c
+++ export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:30:05 by rumachad          #+#    #+#             */
-/*   Updated: 2023/11/10 12:35:38 by rumachad         ###   ########.fr       */
+/*   Updated: 2023/11/23 12:35:39 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	unset(t_env *env, char *cmd);
 
-void	export(t_env *env, char **cmd_split)
+void	export(t_env *env, char *cmd_split)
 {
 	char	*var;
 	int		k;
@@ -22,17 +22,17 @@ void	export(t_env *env, char **cmd_split)
 
 	/* check_export(); */
 	i = 0;
-	while (*cmd_split[i] != '=' && *cmd_split[i])
+	while (cmd_split[i] != '=' && cmd_split[i])
 		i++;
 	var = (char *)malloc(sizeof(char) * (i + 1));
 	k = -1;
 	while (++k < i)
-		var[k] = *cmd_split[k];
+		var[k] = cmd_split[k];
 	var[k] = '\0';
 	//SEG_FAULT Quando o argmento: a =0
 	if (get_env(env, var))
 		unset(env, var);
 	env = env_last(env);
-	env->next = create_node(var, *cmd_split);
+	env->next = create_node(var, cmd_split);
 	free(var);
 }
